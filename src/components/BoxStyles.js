0d0845d11e42afe4b1e219d2fc75666d7d6177ea
DavidGeorge <Davidgeorgechigoziri@gmail.com>
2025-05-16 13:15:59 +0100

    style: Update BoxStyles component for improved color contrast and readability

--- src/components/BoxStyles.js
+++ src/components/BoxStyles.js
@@ -21,8 +21,8 @@ return (
                     </button>
                 </div>
 
-                <div className="w-full md:w-1/3 h-fit bg-blue-800 rounded-lg p-6 md:p-10 mx-0 md:mx-5 my-5">
-                    <h1 className="text-2xl md:text-4xl text-white">
+                <div className="w-full md:w-1/3 h-fit md:bg-blue-800 bg-black rounded-lg p-6 md:p-10 mx-0 md:mx-5 my-5">
+                    <h1 className="text-2xl md:text-4xl text-blue-800 md:text-white">
                         Find a Homecell <br />Near You
                     </h1>
                     <p className="text-base md:text-xl text-white mt-4 md:mt-6">
@@ -44,8 +44,8 @@ return (
                 </div>
             </div>
             <div className="flex flex-col md:flex-row w-full h-full">
-                <div className="w-full md:w-1/3 h-fit bg-blue-800 rounded-lg p-6 md:p-10 mx-0 md:mx-5 my-5">
-                    <h1 className="text-2xl md:text-4xl text-white">Testimony</h1>
+                <div className="w-full md:w-1/3 h-fit md:bg-blue-800 bg-black rounded-lg p-6 md:p-10 mx-0 md:mx-5 my-5">
+                    <h1 className="text-2xl md:text-4xl text-blue-800 md:text-white">Testimony</h1>
                     <p className="text-base md:text-xl text-white mt-4 md:mt-6">
                         Have you been blessed through any of our services? We will love to hear your story! Your testimony can inspire and encourage others in their walk of faith. <br />Click below to share how God has touched your life
                     </p>

1191782e390e4b351b65b1af8800da2255762602
Dave <Davidgeorgechigoziri@gmail.com>
2025-05-29 13:16:01 +0100

    feat: Replace old image assets in Hero component with new files

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
-import image1 from '../assets/first.png'
-import image2 from '../assets/second.png'
+import image1 from '../assets/firs.png'
+import image2 from '../assets/secon.png'
 import image3 from '../assets/third.jpg'
 import { FaArrowRight } from 'react-icons/fa'
 import { AnimatePresence, motion } from 'framer-motion'
@@ -37,6 +37,7 @@ const Hero = () => {
       } else {
         setSlideIndex(0)
       }
+
      };
 
      useEffect(() => {
